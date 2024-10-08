28e73d8515f0ee1c239cdc0e6c89be74d83f7c9a
Sikander-Mirza <sikandersunny2017@gmail.com>
2024-10-08 09:54:43 -0700

    mongoDB

--- src/Infractructure/db.mjs
+++ src/Infractructure/db.mjs
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const ConnectDB = async ()=>{
-  const uri = 'mongodb://localhost:27017/';
+  const uri = 'mongodb+srv://sikandersunny2017:ywFTVMUfxmbyUzKi@cluster0.qpf0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
   // Connect to MongoDB using Mongoose (without deprecated options)
   mongoose.connect(uri,{
