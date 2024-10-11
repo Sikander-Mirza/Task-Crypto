7cdf84d9ce345cb760ed14f77c09d0746dd7fa24
Sikander-Mirza <sikandersunny2017@gmail.com>
2024-10-10 16:25:50 -0800

    /

--- src/Routes/stockRoute.mjs
+++ src/Routes/stockRoute.mjs
@@ -2,7 +2,7 @@ import Stock from "../Controllers/stockController.mjs";
 
 const Routes =async(app) => {
      
-    app.post('/createstock', upload.single('file'), Stock.createstock); 
+    // app.post('/createstock', upload.single('file'), Stock.createstock); 
     app.get("/getstock",Stock.Getstock)
 
 };
